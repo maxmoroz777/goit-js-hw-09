431c6aaa7089331f663ef33b1f2a7137fb70e81c
Max Moroz <maksim.moroz@gmail.com>
2024-07-06 12:13:29 +0100

    up

---
+++ src/js/2-form.js
@@ -0,0 +1,48 @@
+const formData = {
+    email: "",
+    message: "",
+};
+
+const mainForm = document.querySelector(".feedback-form");
+
+mainForm.addEventListener("input", evt => {
+    evt.preventDefault();
+
+    if (evt.target.name === "email") {
+        formData.email = evt.target.value.trim();
+    }
+
+    if (evt.target.name === "message") {
+        formData.message = evt.target.value.trim();
+    }
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+});
+
+const emailInput = document.getElementsByName("email");
+const messageInput = document.getElementsByName("message");
+const localStorageData = localStorage.getItem("feedback-form-state");
+
+const fillForm = () => {
+    if (localStorageData) {
+        const parseData = JSON.parse(localStorageData);
+        formData.email = parseData.email;
+        formData.message = parseData.message;
+        emailInput[0].value = formData.email;
+        messageInput[0].value = formData.message;
+    }
+}
+
+mainForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (!formData.email || !formData.message) {
+        alert("Fill please all fields");
+        return;
+    }
+
+    console.log(formData);
+    localStorage.removeItem("feedback-form-state");
+    formData.email = "";
+    formData.message = "";
+    mainForm.reset();
+});
